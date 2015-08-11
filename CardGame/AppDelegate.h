a18c068a4721ff8e4081b34ceb66131ba8cab104
Wei Tu <weitu@Weis-MacBook-Pro.local>
2015-08-11 23:03:53 +0800

    Initial Commit

---
+++ CardGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CardGame
+//
+//  Created by Wei Tu on 11/8/15.
+//  Copyright (c) 2015 Wei Tu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
